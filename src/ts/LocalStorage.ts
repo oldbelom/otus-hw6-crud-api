a9231e4efde7541e7b1246d97b745a84ca4d6e5a
oldbelom <oldbelom@gmail.com>
2021-11-26 11:27:02 +0300

    add await into LocalStorage methods

--- src/ts/LocalStorage.ts
+++ src/ts/LocalStorage.ts
@@ -2,10 +2,12 @@ import { CRUD, ITask } from "./CRUD";
 
 export class LocalStorage extends CRUD {
   createItem = async (task: ITask, taskKey: string): Promise<void> => {
+    await null;
     localStorage.setItem(taskKey, JSON.stringify(task));
   };
 
   readItem = async (taskKey: string): Promise<string> => {
+    await null;
     if (localStorage.getItem(taskKey) === null) {
       console.log("No matching item");
     }
@@ -18,12 +20,14 @@ export class LocalStorage extends CRUD {
     newValue: string,
     taskKey: string
   ): Promise<void> => {
+    await null;
     const newTask = task;
     newTask[updatedFiekd] = newValue;
     localStorage.setItem(taskKey, JSON.stringify(newTask));
   };
 
   deleteItem = async (taskKey: string): Promise<void> => {
+    await null;
     localStorage.removeItem(taskKey);
   };
 }
